abc32fc258add505458379b2a8acf599cc72d8c8
sidindia <53150104+sidindia@users.noreply.github.com>
2019-07-21 23:24:04 +0530

    Add files via upload

---
+++ challenges_1.java
@@ -0,0 +1,29 @@
+public class prefix
+{
+	public static void main(String[] args) {
+	    System.out.println(longestCommonPrefix(new String[]{"Hello","Hi","Highest"}));
+	}
+	
+	private static String longestCommonPrefix(String[] array)
+	{
+	    String prefix = "";
+	    if(array == null || array.length == 0) 
+	    {
+	        return "";
+	    }
+	    prefix = array[0];
+	    
+	    for(int index = 1; index < array.length; index++)
+	    {
+	        for(int count = 1; count < array[index].length(); count++)
+	        {
+	            if(!array[index].substring(0,count).equals(prefix.substring(0,count)))
+	            {
+	                prefix = prefix.substring(0, count -1);
+	                break;
+	            }
+	        }
+	    }
+	    return prefix;
+	}
+}
