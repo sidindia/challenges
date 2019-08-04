fd845caa4945413345c490ccfad90ad55a1c5e28
sidindia <53150104+sidindia@users.noreply.github.com>
2019-08-05 00:09:52 +0530

    Update challenges_3.java

--- challenges_3.java
+++ challenges_3.java
@@ -14,7 +14,7 @@ public class Main
 {
 	public static void main(String[] args) {
 		int array[][] = {
-		    {1,  2,  3,  4,  5},
+		    	{1,  2,  3,  4,  5},
 			{6,  7,  8,  9,  10},
 			{11, 12, 13, 14, 15},
 			{16, 17, 18, 19, 20},
